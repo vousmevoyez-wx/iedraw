ed6d92d73ff8c55935b8ca26e0745a905f559545
wangxi <wxx_ss@163.com>
2019-07-30 12:32:29 +0800

    修改LoginController

--- plat-parent/plat-service/src/main/java/com/shengyuanjun/iedraw/web/controller/LoginController.java
+++ plat-parent/plat-service/src/main/java/com/shengyuanjun/iedraw/web/controller/LoginController.java
@@ -17,7 +17,7 @@ public class LoginController {
     @ApiOperation(value="登录请求",tags={"登录操作"},notes="要有用户名和密码")
     @RequestMapping(value = "/login",method = RequestMethod.POST)
     public AjaxResult login(@RequestBody User user){
-        if("admin".equals(user.getUsername())&&"0".equals(user.getPassword())){
+        if("admin".equals(user.getUsername())&&"admin".equals(user.getPassword())){
             return AjaxResult.me().setMessage("登录成功！").setRetObj(user);
         }
         return AjaxResult.me().setSuccess(false).setMessage("用户名或密码错误!");
