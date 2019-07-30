be7fdf5b1367e242e0a5b2e433753ace3dc0c69a
wangxi <wxx_ss@163.com>
2019-07-30 16:49:02 +0800

    搭建平台服务、抽奖服务、代码生成器、配置服务、基础代码服务框架

---
+++ code-generator/src/main/java/com/shengyuanjun/iedraw/GenteratorCode.java
@@ -0,0 +1,109 @@
+package com.shengyuanjun.iedraw;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+import java.util.*;
+
+public class GenteratorCode {
+
+    public static void main(String[] args) throws InterruptedException {
+        //用来获取Mybatis-Plus.properties文件的配置信息
+        //是一个配置文件内容的读取，文件名为mybatis-plus-xx.properties
+        ResourceBundle rb = ResourceBundle.getBundle("mybatis-plus-iedraw");
+        AutoGenerator mpg = new AutoGenerator();
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir(rb.getString("OutputDir"));
+        gc.setFileOverride(true);
+        gc.setActiveRecord(true);// 开启 activeRecord 模式
+        gc.setEnableCache(false);// XML 二级缓存
+        gc.setBaseResultMap(true);// XML ResultMap
+        gc.setBaseColumnList(false);// XML columList
+        gc.setAuthor(rb.getString("author"));
+        mpg.setGlobalConfig(gc);
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setTypeConvert(new MySqlTypeConvert());
+        dsc.setDriverName(rb.getString("jdbc.driver"));
+        dsc.setUsername(rb.getString("jdbc.user"));
+        dsc.setPassword(rb.getString("jdbc.pwd"));
+        dsc.setUrl(rb.getString("jdbc.url"));
+        mpg.setDataSource(dsc);
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setTablePrefix(new String[] { "t_" });// 此处可以修改为您的表前缀
+        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+        strategy.setInclude(new String[]{"t_..."}); // 需要生成的表t_xx
+        mpg.setStrategy(strategy);
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent(rb.getString("parent"));
+        pc.setController("controller");
+        pc.setService("service");
+        pc.setServiceImpl("service.impl");
+        pc.setEntity("domain");
+        pc.setMapper("mapper");
+        mpg.setPackageInfo(pc);
+
+        // 注入自定义配置，可以在 VM 中使用 cfg.abc 【可无】
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-rb");
+                this.setMap(map);
+            }
+        };
+
+        List<FileOutConfig> focList = new ArrayList<FileOutConfig>();
+
+        // 自定义 domain 的生成策略
+        focList.add(new FileOutConfig("/templates/entity.java.vm") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                return rb.getString("OutputDirBase")+ "/com/shengyuanjun/iedraw/domain/" + tableInfo.getEntityName() + ".java";
+            }
+        });
+
+        // 自定义 query 的生成策略
+        focList.add(new FileOutConfig("/templates/query.java.vm") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                return rb.getString("OutputDirBase")+ "/com/shengyuanjun/iedraw/query/" + tableInfo.getEntityName() + "Query.java";
+            }
+        });
+
+        // 调整 xml 生成目录演示
+        focList.add(new FileOutConfig("/templates/mapper.xml.vm") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                return rb.getString("OutputDirXml")+ "/com/shengyuanjun/iedraw/mapper/" + tableInfo.getEntityName() + "Mapper.xml";
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/templates 下面内容修改，
+        // 放置自己项目的 src/main/resources/templates 目录下, 默认名称一下可以不配置，也可以自定义模板名称
+        TemplateConfig tc = new TemplateConfig();
+        tc.setService("/templates/service.java.vm");
+        tc.setServiceImpl("/templates/serviceImpl.java.vm");
+        tc.setEntity(null);
+        tc.setMapper("/templates/mapper.java.vm");
+        tc.setController("/templates/controller.java.vm");
+        tc.setXml(null);
+        // 如上任何一个模块如果设置 空 OR Null 将不生成该模块。
+        mpg.setTemplate(tc);
+
+        // 执行生成
+        mpg.execute();
+    }
+
+}
